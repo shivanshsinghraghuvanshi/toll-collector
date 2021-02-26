a48d15fa947256028ef15a936b2f7c4ed1529418
Shivansh Raghuvanshi <ssinghraghuvanshi@deloitte.com>
2021-02-26 19:49:52 +0530

    Added tolltax service

---
+++ tolltax/up.sql
@@ -0,0 +1,21 @@
+Create table if not exists owner{
+    ownerid INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(40) not null ,
+    primary key(ownerid)
+    }
+
+create table if not exists car{
+    carid INT GENERATED ALWAYS AS IDENTITY,
+    make varchar (30),
+    cartype varchar (10) not null,
+    carnumber varchar (12) not null,
+    primary key(carid)
+    }
+
+create table if not exists netc{
+    netcid INT GENERATED ALWAYS AS IDENTITY,
+    primary key(netcid)
+    rfid varchar (50) not null ,
+    constraint fkownerid foreign key(ownerid) references owner(ownerid)
+    constraint fkcarid foreign key(carid) references car(carid)
+    }
