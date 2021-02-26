d8843761008fb00a6849af25dcaf5bd62ed1a391
Shivansh Raghuvanshi <ssinghraghuvanshi@deloitte.com>
2021-02-26 23:58:41 +0530

    TollTax Service Last Stand

--- tolltax/server.go
+++ tolltax/server.go
@@ -1,6 +1,7 @@
 package tolltax
 
 import (
+	"context"
 	"fmt"
 	"github.com/shivanshsinghraghuvanshi/toll-collector/tolltax/pb/tolltaxpb"
 	"google.golang.org/grpc"
@@ -12,13 +13,33 @@ type grpcServer struct {
 	service Service
 }
 
+func (g grpcServer) GenerateRFID(ctx context.Context, request *tolltaxpb.GenerateRFIDRequest) (*tolltaxpb.GenerateRFIDResponse, error) {
+	panic("implement me")
+}
+
+func (g grpcServer) ValidateRFID(ctx context.Context, request *tolltaxpb.ValidateRFIDRequest) (*tolltaxpb.ValidateRFIDResponse, error) {
+	panic("implement me")
+}
+
+func (g grpcServer) DeductTransaction(ctx context.Context, request *tolltaxpb.DeductRequest) (*tolltaxpb.DeductResponse, error) {
+	panic("implement me")
+}
+
+func (g grpcServer) CreditTransaction(ctx context.Context, request *tolltaxpb.CreditRequest) (*tolltaxpb.CreditResponse, error) {
+	panic("implement me")
+}
+
+func (g grpcServer) CalculateDeductibleAmount(ctx context.Context, request *tolltaxpb.CalculateAmountRequest) (*tolltaxpb.CalculateAmountResponse, error) {
+	panic("implement me")
+}
+
 func ListenGRPC(s Service, port int) error {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		return err
 	}
 	serv := grpc.NewServer()
-	pb.Regis
+	tolltaxpb.RegisterTollTaxServiceServer(serv, &grpcServer{s})
 	reflection.Register(serv)
 	return serv.Serve(lis)
 }
