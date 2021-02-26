5c688e9060250f4424f70b7931b03468380d9c00
Shivansh Raghuvanshi <ssinghraghuvanshi@deloitte.com>
2021-02-27 00:16:37 +0530

    vendor directoty

--- tolltax/client.go
+++ tolltax/client.go
@@ -1,1 +1,24 @@
 package tolltax
+
+import (
+	"github.com/shivanshsinghraghuvanshi/toll-collector/tolltax/pb/tolltaxpb"
+	"google.golang.org/grpc"
+)
+
+type Client struct {
+	conn    *grpc.ClientConn
+	service tolltaxpb.TollTaxServiceClient
+}
+
+func NewClient(url string) (*Client, error) {
+	conn, err := grpc.Dial(url, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	c := tolltaxpb.NewTollTaxServiceClient(conn)
+	return &Client{conn, c}, nil
+}
+
+func (c *Client) Close() {
+	c.conn.Close()
+}
